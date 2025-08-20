48803c4e103904a391bfed44b7e5672674030e0e
Pedro Luiz <peuluiz.13@gmail.com>
2025-08-20 07:59:39 -0300

    ajustes

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Header } from "./components/header";
 import { OrderProvider } from "@/providers/order";
 
+//isso força esse layout e todas as páginas filhas a serem dinâmicas
+export const dynamic = "force-dynamic";
+
 export default function DashboardLayout({
   children,
 }: {
